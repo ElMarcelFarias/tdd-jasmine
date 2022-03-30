061670b867392d8e82811be2288efb7a9cc3baa1
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-03-30 20:56:53 -0300

    new commit toThrowError

--- spec/matchers-spec.js
+++ spec/matchers-spec.js
@@ -157,3 +157,20 @@ describe("Suíte de testes do toThrow", function() {
         expect(semErro).not.toThrow();
     });
 });
+
+describe("Suíte de testes do tópico 5.13", function() {
+    var calcularDobro = function(numero) {
+        if(numero <=0) {
+            throw new TypeError("O número deve ser maior que 0.")
+        }
+        return numero * numero;
+    };
+    it("deve validar o uso do matcher 'toThrowError'", function() {
+        expect(function() {calcularDobro(0)}).toThrowError();
+        expect(function() {calcularDobro(0)}).toThrowError("O número deve ser maior que 0.");
+        expect(function() {calcularDobro(0)}).toThrowError(/maior que 0/);
+        expect(function() {calcularDobro(0)}).toThrowError(TypeError);
+        expect(function() {calcularDobro(0)}).toThrowError(TypeError, "O número deve ser maior que 0.");
+        expect(calcularDobro).not.toThrowError();
+    });
+});
